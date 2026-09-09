6ca387bc9afa8e00fb5ab653c8fd44fd5918dc27
lnando2009 <41671@raphaeldisanto.com.br>
2026-09-09 11:28:58 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ from routes.user_routes import user_bp
 from routes.formulario_routes import formulario_bp
 
 app = Flask(__name__)
+
 app.config.from_pyfile('config.py')
+
 jwt = JWTManager(app)
 
 init_db()
 
-app.register_blueprint(user_bp, url_prefix='/user')
-app.register_blueprint(formulario_bp,url_prefix='/formularios')
+app.register_blueprint(user_bp, url_prefix='/users')
+app.register_blueprint(formulario_bp, url_prefix='/formularios')
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
